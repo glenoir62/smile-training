d143ff292abfaa6474a1cd19e1db7f213eabdd68
Guillaume Lenoir <guillaume.lenoir@gleo.fr>
2018-05-28 01:49:51 +0200

    update

--- modules/Advanced-web/src/main/java/fr/smile/training/web/portlet/configurationicon/SamplePortletConfigurationIcon.java
+++ modules/Advanced-web/src/main/java/fr/smile/training/web/portlet/configurationicon/SamplePortletConfigurationIcon.java
@@ -1,23 +1,44 @@
 package fr.smile.training.web.portlet.configurationicon;
 
-import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import java.util.ResourceBundle;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 
 import org.osgi.service.component.annotations.Component;
 
-/**
- * @author guillaumelenoir
- *
- */
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
 @Component(
 		immediate = true,
-		property = {"javax.portlet.name=fr_smile_training_web_advanced_portlet_AdvancedPortlet"},
-		service = SamplePortletConfigurationIcon.class
+		property = "javax.portlet.name=fr_smile_training_web_advanced_portlet_AdvancedPortlet",
+		service = PortletConfigurationIcon.class
 	)
 public class SamplePortletConfigurationIcon extends BasePortletConfigurationIcon {
-	
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", getLocale(portletRequest), getClass());
+
+		return LanguageUtil.get(resourceBundle, "sample-link");
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return "https://www.liferay.com";
+	}
+
+	@Override
+	public double getWeight() {
+		return 150.0;
+	}
+
 	@Override
 	public boolean isShow(PortletRequest portletRequest) {
 		return true;
@@ -28,37 +49,10 @@ public class SamplePortletConfigurationIcon extends BasePortletConfigurationIcon
 		return false;
 	}
 
-	
-	
 	@Override
 	public boolean isUseDialog() {
 		return false;
 	}
 
-	@Override
-	public String getMessage(PortletRequest portletRequest) {
-		return "custom-icon";
-	}
-
-	@Override
-	public String getMethod() {
-		return "GET";
-	}
-
-	@Override
-	public String getURL(PortletRequest portletRequest, PortletResponse portletResponse) {
-		// TODO Auto-generated method stub
-		return "wwww.google.fr";
-	}
-
-	@Override
-	public double getWeight() {
-		return 1l;
-	}
-
-	
-	
-
-
-
 }
+
